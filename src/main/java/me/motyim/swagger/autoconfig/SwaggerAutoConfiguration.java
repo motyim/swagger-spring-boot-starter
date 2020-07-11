0507f8b02ca7f4d1f1a5b7db90ab44e10bff55e6
Mohamed MotYim <mohamed.motyim@gmail.com>
2020-07-11 21:58:23 +0200

    add swagger properties

--- src/main/java/me/motyim/swagger/autoconfig/SwaggerAutoConfiguration.java
+++ src/main/java/me/motyim/swagger/autoconfig/SwaggerAutoConfiguration.java
@@ -1,14 +1,77 @@
 package me.motyim.swagger.autoconfig;
 
+import me.motyim.swagger.properties.DocketProperty;
+import me.motyim.swagger.properties.InfoProperties;
 import me.motyim.swagger.properties.SwaggerProperties;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.plugin.core.OrderAwarePluginRegistry;
+import org.springframework.plugin.core.PluginRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.DocumentationPlugin;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Map;
 
 @Configuration
 @EnableSwagger2
 @EnableConfigurationProperties(SwaggerProperties.class)
 @ConditionalOnProperty(prefix = "spring.swagger", name = "enable", havingValue = "true", matchIfMissing = true)
 public class SwaggerAutoConfiguration {
+
+    private final SwaggerProperties properties;
+
+    private final Logger log = LoggerFactory.getLogger(SwaggerAutoConfiguration.class);
+
+    @Autowired
+    public SwaggerAutoConfiguration(SwaggerProperties properties) {
+        log.info("Swagger Starter Enabled : {}",properties.isEnable());
+        this.properties = properties;
+    }
+
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Bean
+    @Primary
+    @Qualifier("documentationPluginRegistry")
+    public PluginRegistry<DocumentationPlugin, DocumentationType> swaggerDocumentationPluginRegistry(List<DocumentationPlugin> plugins) {
+
+        InfoProperties appInfo = properties.getInfo();
+
+        log.debug("app info : {}", appInfo);
+
+
+        Map<String, DocketProperty> dockets = properties.getDockets();
+        dockets.forEach((key, value) -> log.debug("Key : {} , value {}", key, value));
+
+
+        properties.getDockets().forEach(
+                (key, value) -> {
+                    Docket build = new Docket(DocumentationType.SWAGGER_2)
+                            .groupName(key)
+                            .apiInfo(value.getInfo().get())
+                            .select()
+                            .apis(RequestHandlerSelectors.basePackage(value.getBasePackage()))
+                            .paths(PathSelectors.ant(value.getPath()))
+                            .build();
+                    plugins.add(build);
+
+                }
+        );
+
+        log.info("Register swagger-dockets");
+        return OrderAwarePluginRegistry.create(plugins);
+    }
+    
 }
--- src/main/java/me/motyim/swagger/properties/SwaggerProperties.java
+++ src/main/java/me/motyim/swagger/properties/SwaggerProperties.java
@@ -4,15 +4,22 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Data
 @ConfigurationProperties(prefix = "spring.swagger")
 public class SwaggerProperties {
 
     private boolean enable = true;
+    private boolean redirect;
 
     @NestedConfigurationProperty
     private InfoProperties info;
+
+    @NestedConfigurationProperty
+    private Map<String, DocketProperty> dockets = new HashMap<>();
 
     public SwaggerProperties() {
         this.info = new InfoProperties();
